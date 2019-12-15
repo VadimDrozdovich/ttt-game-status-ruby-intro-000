5d2935629d599fb50f76386b296e988ede14734d
Vadym Drozdovych <vadim.drozdovich@gmail.com>
2019-12-15 17:22:57 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,16 +1,20 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 WIN_COMBINATIONS = [
-  [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,7], [6,4,2], [0, 4, 8]
-]
+  [0,1,2], 
+  [3,4,5], 
+  [6,7,8], 
+  [0,3,6], 
+  [1,4,7], 
+  [2,5,8], 
+  [0,4,8], 
+  [6,4,2], 
+  ]
 
-if board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board[top_row_win[2]] == "X"
-  puts "won"
-elsif board[middle_row_win[3]] == "X" && board[top_row_win[5]] == "X" && board[top_row_win[5]] == "X"
-  puts "won"
-elsif board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board[top_row_win[2]] == "X"
+win_index_1 = win_combination[0]
+win_index_2 = win_combination[1]
+win_index_3 = win_combination[2]
 
-
-
-
-# Define your WIN_COMBINATIONS constant
+position_1 = board[win_index_1]
+position_2 = board[win_index_2]
+position_3 = board[win_index_3]# Define your WIN_COMBINATIONS constant
