81b858c10106e9b128fa3eaff4ad88bb6fa88c20
Vadym Drozdovych <vadim.drozdovich@gmail.com>
2019-12-15 17:06:29 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,16 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,7], [6,4,2]]
+WIN_COMBINATIONS = [
+  [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,7], [6,4,2]
+]
+
+if board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board[top_row_win[2]] == "X"
+  puts "won"
+elsif board[middle_row_win[3]] == "X" && board[top_row_win[5]] == "X" && board[top_row_win[5]] == "X"
+  puts "won"
+elsif board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" && board[top_row_win[2]] == "X"
+  
+      
 
 
 # Define your WIN_COMBINATIONS constant
