ca7acd89e6ea59fb9097c661b85a838a4c63f1f5
Vadym Drozdovych <vadim.drozdovich@gmail.com>
2019-12-15 16:54:57 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,6 @@
-# Helper Method
-def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " ")
-end
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,7], [6,4,2]]
+
 
 # Define your WIN_COMBINATIONS constant
